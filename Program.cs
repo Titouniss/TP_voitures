c76a20a9b3a8002165055c3c53c03c713d6310dd
thais53 <villanne.thais@gmail.com>
2021-02-04 16:02:01 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TP_01_BLOC2
+{
+    class Program
+    {
+        static void Main (string[] args)
+            
+        {
+            Voiture R1 = new Voiture();
+            R1.SetMarque("");
+            R1.SetCouleur("");
+            R1.SetMatricule("");
+            R1.SetChevaux(0);
+            R1.GetNumero();
+            Console.WriteLine(R1.presentation());
+
+
+            Voiture R2 = new Voiture();
+            R2.SetMarque("");
+            R2.SetCouleur("");
+            R2.SetMatricule("");
+            R2.SetChevaux(0);
+            R2.GetNumero();
+            Console.WriteLine(R2.presentation());
+
+        }
+    }
+}
---
+++ voiture.cs
@@ -0,0 +1,87 @@
+﻿namespace TP_01_BLOC2
+{
+    internal class Voiture
+    {
+        private string marque;
+        private string couleur;
+        private string matricule;
+        private int chevaux;
+        private int numero;
+        private static int autoincrementation = 0;
+
+        public Voiture()
+        {
+            autoincrementation++;
+            this.numero = autoincrementation;
+        }
+
+        public Voiture(string marq, string coul, string mat, int chev)
+        {
+            this.marque = marq;
+            this.couleur = coul;
+            this.matricule = mat;
+            this.chevaux = chev;
+            autoincrementation++;
+            this.numero = autoincrementation;
+        }
+
+        public Voiture(string mat)
+        {
+            this.matricule = mat;
+        }
+
+        public string GetMarque()
+        {
+            return this.marque;
+        }
+
+        public string GetCouleur()
+        {
+            return this.couleur;
+        }
+
+        public string GetMatricule()
+        {
+            return this.matricule;
+        }
+
+        public int GetChevaux()
+        {
+            return this.chevaux;
+        }
+
+        public int GetNumero()
+        {
+            return this.numero;
+        }
+
+        public void SetMarque(string marque)
+        {
+            this.marque = marque;
+        }
+
+        public void SetCouleur(string couleur)
+        {
+            this.couleur = couleur;
+        }
+
+        public void SetMatricule(string matricule)
+        {
+            this.matricule = matricule;
+        }
+
+        public void SetChevaux(int chevaux)
+        {
+            this.chevaux = chevaux;
+        }
+
+        public string presentation()
+        {
+            string resultat = "la voiture est de la marque " + this.marque;
+            resultat += "  sa couleur est " + this.couleur;
+            resultat += "  son matricule est le :" + this.matricule;
+            resultat += "  elle possède " + this.chevaux + " chevaux "; 
+            return resultat;
+        }
+    }
+}
